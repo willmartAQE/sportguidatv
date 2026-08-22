871996eaa97ec7ca86cbf0dbf4a713b517aa71ba
willmartAQE <williammartin.AQE@gmail.com>
2026-08-22 23:29:25 +0100

    Integrate Horse TV schedule refresh

--- netlify/functions/refresh-schedule.js
+++ netlify/functions/refresh-schedule.js
@@ -1,9 +1,10 @@
 import { fetchSkyEvents } from '../../src/scrapers/sky.js';
 import { fetchDaznFootballEvents } from '../../src/scrapers/dazn.js';
+import { fetchHorseTvEvents } from '../../src/scrapers/horse-tv.js';
 import { setCache } from '../../src/storage/cache.js';
 
 export default async () => {
-  const results = await Promise.allSettled([fetchSkyEvents(), fetchDaznFootballEvents()]);
+  const results = await Promise.allSettled([fetchSkyEvents(), fetchDaznFootballEvents(), fetchHorseTvEvents()]);
   const events = results.flatMap((result) => result.status === 'fulfilled' ? result.value.events : []);
   const errors = results.flatMap((result) => result.status === 'rejected' ? [result.reason.message] : []);
   await setCache(events);
