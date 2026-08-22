50c01a3eb569bd00de3908d61a53aacd68ef09c9
willmartAQE <williammartin.AQE@gmail.com>
2026-08-22 23:15:46 +0100

    Add equitazione sport button

--- src/bot/keyboard.js
+++ src/bot/keyboard.js
@@ -2,6 +2,7 @@ export const sportKeyboard = () => ({ inline_keyboard: [
   [{ text: '⚽ Calcio', callback_data: 'sport:calcio' }, { text: '🎾 Tennis', callback_data: 'sport:tennis' }],
   [{ text: '🏎 Formula 1', callback_data: 'sport:f1' }, { text: '🏍 MotoGP', callback_data: 'sport:motogp' }],
   [{ text: '🏀 Basket', callback_data: 'sport:basket' }, { text: '🏐 Volley', callback_data: 'sport:volley' }],
+  [{ text: '🐎 Equitazione', callback_data: 'sport:equitazione' }],
   [{ text: '📅 Domani', callback_data: 'day:tomorrow' }, { text: '🔄 Aggiorna', callback_data: 'day:today' }]
 ] });
 
